97635461e9b91064a126e21ce1644cc5e285cf42
Janitz_De_Paz <janitzdp@gmail.com>
2026-03-25 13:23:30 +0000

    arreglar conflicto merge

--- src/model/MesaRedonda.java
+++ src/model/MesaRedonda.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //11/02/2026 14:02
 package model;
 import java.time.LocalDate;
@@ -31,49 +30,6 @@ public class MesaRedonda extends Evento {
    //TOSTRING
    @Override
    public String toString() {
-       return super.toString() + "\n Nº Conferencia: '" + this.getnConferencia() + '\'';
+       return super.toString() + "\n NÂº Conferencia: '" + this.getnConferencia() + '\'';
    };
 }
-
-
-
-=======
-//11/02/2026 14:02
-package model;
-import java.time.LocalDate;
-import java.time.LocalTime;
-public class MesaRedonda extends Evento {
-   private int nConferencia;
-   private static int conferenciasCreadas;
-   //CONSTRUCTORES
-   public MesaRedonda() {
-       super();
-       conferenciasCreadas++;
-   }
-   public MesaRedonda(String titulo, String ubicacion, String descripcion, LocalDate fIni, LocalDate fFin,
-           LocalTime hIni, LocalTime hFin, String codigo, int nConferencia) {
-       super(titulo, ubicacion, descripcion, fIni, fFin, hIni, hFin, codigo);
-       this.nConferencia = nConferencia;
-       conferenciasCreadas++;
-   }
-   //GETTERS
-   public int getnConferencia() {
-       return nConferencia;
-   }
-   public void setnConferencia(int nConferencia) {
-       this.nConferencia = nConferencia;
-   }
-   public static int getConferenciasCreadas() {
-       return conferenciasCreadas;
-   }
-  
-   //TOSTRING
-   @Override
-   public String toString() {
-       return super.toString() + "\n Nº Conferencia: '" + this.getnConferencia() + '\'';
-   };
-}
-
-
-
->>>>>>> 75a10faca468507b1ae6581801322b6b45331215
