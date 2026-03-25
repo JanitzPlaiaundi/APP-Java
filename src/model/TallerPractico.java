357b5091910a01e8c132dd384eaf2795dc25a5a5
Abel Redero <177021990+Salomon3534@users.noreply.github.com>
2026-03-25 14:22:30 +0100

    arreglo chapuza aaron

--- src/model/TallerPractico.java
+++ src/model/TallerPractico.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-//13/02/2026 10:33
+
 package model;
 
 import java.time.LocalDate;
@@ -36,52 +35,6 @@ public class TallerPractico extends Evento {
     
     @Override
     public String toString() {
-        return super.toString() + "\n  Nº taller: '" + this.getnTaller() + '\'';
+        return super.toString() + "\n  NÂº taller: '" + this.getnTaller() + '\'';
     };
 }
-
-
-=======
-//13/02/2026 10:33
-package model;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-
-public class TallerPractico extends Evento {
-    private int nTaller;
-    private static int talleresPracticosCreados;
-
-    public TallerPractico() {
-        super();
-        talleresPracticosCreados++;
-    }
-
-    public TallerPractico(String titulo, String ubicacion, String descripcion, LocalDate fIni, LocalDate fFin,
-            LocalTime hIni, LocalTime hFin, String codigo, int nTaller) {
-        super(titulo, ubicacion, descripcion, fIni, fFin, hIni, hFin, codigo);
-        this.nTaller = nTaller;
-        talleresPracticosCreados++;
-    }
-
-    public int getnTaller() {
-        return nTaller;
-    }
-
-    public void setnTaller(int nTaller) {
-        this.nTaller = nTaller;
-    }
-
-    public static int getTalleresPracticosCreados() {
-        return talleresPracticosCreados;
-    }
-    
-    @Override
-    public String toString() {
-        return super.toString() + "\n  Nº taller: '" + this.getnTaller() + '\'';
-    };
-}
-
-
->>>>>>> 75a10faca468507b1ae6581801322b6b45331215
