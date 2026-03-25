f7290f02e450cbb5d23b20edc84ebb11a4f2c3bd
Abel Redero <177021990+Salomon3534@users.noreply.github.com>
2026-03-25 14:23:29 +0100

    arreglo chapuza aaron

--- src/model/PresentacionProyecto.java
+++ src/model/PresentacionProyecto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //13/02/2026 10:32
 package model;
 import java.time.LocalDate;
@@ -39,51 +38,3 @@ public class PresentacionProyecto extends Evento {
        return super.toString() + "\n  Tipo proyecto: '" + this.getTipoProyecto() + '\'' + "\n  Descripcion proyecto: '" + this.getDescripcionProyecto() + '\'';
    };
 }
-
-
-
-=======
-//13/02/2026 10:32
-package model;
-import java.time.LocalDate;
-import java.time.LocalTime;
-public class PresentacionProyecto extends Evento {
-   private String tipoProyecto;
-   private String descripcionProyecto;
-   private static int presentacionesCreadas;
-   public PresentacionProyecto() {
-       super();
-       presentacionesCreadas++;
-   }
-   public PresentacionProyecto(String titulo, String ubicacion, String descripcion, LocalDate fIni, LocalDate fFin,
-           LocalTime hIni, LocalTime hFin, String codigo, String tipoProyecto, String descripcionProyecto) {
-       super(titulo, ubicacion, descripcion, fIni, fFin, hIni, hFin, codigo);
-       this.tipoProyecto = tipoProyecto;
-       this.descripcionProyecto = descripcionProyecto;
-       presentacionesCreadas++;
-   }
-   public String getTipoProyecto() {
-       return tipoProyecto;
-   }
-   public void setTipoProyecto(String tipoProyecto) {
-       this.tipoProyecto = tipoProyecto;
-   }
-  
-   public String getDescripcionProyecto() {
-       return descripcionProyecto;
-   }
-   public void setDescripcionProyecto(String descripcionProyecto) {
-       this.descripcionProyecto = descripcionProyecto;
-   }
-   public static int getPresentacionesCreadas() {
-       return presentacionesCreadas;
-   }
-   @Override
-   public String toString() {
-       return super.toString() + "\n  Tipo proyecto: '" + this.getTipoProyecto() + '\'' + "\n  Descripcion proyecto: '" + this.getDescripcionProyecto() + '\'';
-   };
-}
-
-
-
->>>>>>> 75a10faca468507b1ae6581801322b6b45331215
